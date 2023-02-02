cd9e1f1acdf75d1405e0cb9e6ca577aca23b727a
Nemesis <lorenzo.balugani@gmail.com>
2023-02-02 09:12:28 +0100

    :sparkles: Parallel preview for deliverers

--- Applications/acmeat/services/messageTest.py
+++ Applications/acmeat/services/messageTest.py
@@ -2,6 +2,6 @@ from pycamunda.message import CorrelateSingle
 
 url = 'http://localhost:8080/engine-rest'
 a = CorrelateSingle(url, message_name="Message_Payment",
-                    process_instance_id="89b4b6a6-a154-11ed-b359-0242ac110002"
+                    process_instance_id="895130c7-a2d0-11ed-90ee-0242ac110002"
                     )
 a()

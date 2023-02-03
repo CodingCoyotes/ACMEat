60b70ff94c80a0adba71d1176cc25d0ad5459b59
Nemesis <lorenzo.balugani@gmail.com>
2023-02-03 20:18:19 +0100

    :sparkles: Tentative fix

--- acme_front/src/components/Order/ProcessingOrder.js
+++ acme_front/src/components/Order/ProcessingOrder.js
@@ -8,11 +8,11 @@ import OrderProgress from "./OrderProgress";
 import ReactLoading from "react-loading";
 
 const address = "http://127.0.0.1:3001/pay/5ba5f372-78af-4e1f-ad8d-9bfd82020d24/"
-export default function ProcessingOrder(){
+export default function ProcessingOrder() {
     const navigate = useNavigate()
     const {token, setToken} = useAppContext();
     const [user, setUser] = useState(null);
-    const [processState, setProcessState] =useState(0);
+    const [processState, setProcessState] = useState(0);
     const [order, setOrder] = useState(null);
     const {state} = useLocation();
     let {param} = state;
@@ -21,11 +21,13 @@ export default function ProcessingOrder(){
         console.log("processingorder")
         if (token === null) {
             navigate("/")
-        }
-        else if (user===null){
+        } else if (user === null) {
             getInfo();
             getOrd();
-            pooling()
+            const interval = setInterval(() => pooling(), 5000)
+            return () => {
+                clearInterval(interval)
+            }
         }
     }, [])
 
@@ -42,37 +44,34 @@ export default function ProcessingOrder(){
         let response = await getOrder(param, token);
         if (response.status === 200) {
             let values = await response.json();
-                console.log("value status")
-                console.log(values.status);
-                setProcessState(values.status);
-                setOrder(values);
-                return values.status;
+            console.log("value status")
+            console.log(values.status);
+            setProcessState(values.status);
+            setOrder(values);
+            return values.status;
         }
-        return 0;
+        return "0";
     }
 
-    async function pooling(){
+    async function pooling() {
         console.log("pooling")
-        while(getOrd() !== 3){
-            setTimeout(() => {
-                console.log("controllo");
-            }, 5000); //every 5 sec + await time (?)
+        status = await getOrd()
+        if (status === "3") {
+            let reindirizza = address + {param}.restaurant_total + {param}.deliverer_total + "/http:127.0.0.1:3000/landingorder/" + {param}.id
+            window.location.href = reindirizza;
         }
-        console.log("out");
-        let reindirizza = address + {param}.restaurant_total+ {param}.deliverer_total+"/http:127.0.0.1:3000/landingorder/"+{param}.id
-        window.location.href = reindirizza;
-
+        return;
     }
 
 
-    return(
+    return (
         <div className='container'>
-            <div className="card" >
+            <div className="card">
                 <h3 className="Auth-form-title">Informazioni sul tuo ordine</h3>
                 <div className="form-group mt-3">
-                    <OrderProgress orderStatus= {processState}></OrderProgress>
+                    <OrderProgress orderStatus={processState}></OrderProgress>
                     <ReactLoading className="loading" type="spokes" color="#0000FF"
-                                  height={100} width={50} />
+                                  height={100} width={50}/>
                 </div>
             </div>
         </div>
